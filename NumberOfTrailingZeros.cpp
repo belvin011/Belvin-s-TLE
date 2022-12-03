291cb439a73f240c9129916814786f6e4a21319c
MANIKANTA KORIMILLI <112791483+belvin011@users.noreply.github.com>
2022-12-04 05:04:55 +0530

    Add files via upload

---
+++ NumberOfTrailingZeros.cpp
@@ -0,0 +1,15 @@
+//Approch-->
+//-->n/5power1 + n/5power2 + n/5power3 +.......♾️
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	long long n; 
+	cin>>n;
+	long long res=0;
+	long long power5=5;
+	while(n>=power5){    
+		res+=n/power5;   
+		power5*=5;       
+	}
+	cout<<res<<"\n";
+}
