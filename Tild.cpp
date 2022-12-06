25c622153d91e555c745a3715b737efca57ca8c0
MANIKANTA KORIMILLI <112791483+belvin011@users.noreply.github.com>
2022-12-06 11:56:35 +0530

    Add files via upload

---
+++ Tild.cpp
@@ -0,0 +1,12 @@
+//FINDING ELEMENTS WITH REFERENCE TILD~
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="aeiouAEIOU";
+    string ss="manikanta";
+    int c=0;
+    for(int i=0;i<ss.size();i++){
+        if(~s.find(ss[i])) c++;
+    }
+    cout<<c<<"\n";
+}
